5a8d105af834d602e9db2d01e2bc179c20ec593f
jose-cardoso-a22506616 <mrdebugger05@gmail.com>
2026-04-08 10:36:39 +0000

    Entidades Licenciatura, UC, Docente e Projeto feitas.
    Também acrescentei no admin.py estas entidades

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class UCAdmmin(admin.ModelAdmin):
+    list_display = ("nome",)
+    ordering = ("nome",)
+    search_fields = ("nome",)
+
+class LicenciaturaAdmin(admin.ModelAdmin):
+    list_display = ("nome", "descricao")
+    ordering = ("nome",)
+    search_fields = ("nome",)
+
+class DocenteAdmin(admin.ModelAdmin):
+    list_display = ("nome",)
+    ordering = ("nome",)
+    search_fields = ("nome",)
+
+admin.site.register(UC, UCAdmmin)
+admin.site.register(Licenciatura, LicenciaturaAdmin)
+admin.site.register(Docente, DocenteAdmin)
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+
+
+    
+
+
+class Projetos(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=500)
+    repositorio = models.CharField(max_length=100)
+    conceitos = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.nome
+
+
+class Docente(models.Model):
+    nome = models.CharField(max_length=100)
+    mail = models.CharField(max_length=100)
+    site = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+    
+class UC(models.Model):
+    nome = models.CharField(max_length=100)
+    ano = models.IntegerField()
+    semestre = models.IntegerField()
+    docente = models.ManyToManyField(Docente, related_name="ucs")
+    imagem = models.ImageField(upload_to="fotos_uc/", blank=True)
+
+    def __str__(self):
+        return self.nome
+
+
+
+class Licenciatura(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=500)
+    uc = models.ManyToManyField(UC, related_name="licenciaturas", blank=True)
+
+    def __str__(self):
+        return self.nome
