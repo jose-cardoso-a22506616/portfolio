fd8b81cec4054cca56b46b49806d6c2039bc2825
jose-cardoso-a22506616 <mrdebugger05@gmail.com>
2026-04-19 15:57:49 +0000

    visualização de professores, alunos e curos feita

---
+++ escola/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('cursos/', views.cursos_view, name="cursos"),
+    path("", views.cursos_view), # rota que abre diretamente a página dos cursos,
+    path("professores/", views.professores_view, name="professores"),
+    path("alunos/", views.alunos_view, name="alunos"),
+]
--- project/settings.py
+++ project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "portfolio",
+    "escola",
 ]
 
 MIDDLEWARE = [
@@ -120,4 +121,5 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = '/media/'
